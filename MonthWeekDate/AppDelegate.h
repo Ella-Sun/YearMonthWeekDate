b80234f15ed7276f6078804caf8b4b9be34180ad
sunhong <sunhong@ucfgroup.com>
2016-10-26 15:58:27 +0800

    Initial Commit

---
+++ MonthWeekDate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MonthWeekDate
+//
+//  Created by sunhong on 2016/10/26.
+//  Copyright © 2016年 sunhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MonthWeekDate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MonthWeekDate
+//
+//  Created by sunhong on 2016/10/26.
+//  Copyright © 2016年 sunhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
